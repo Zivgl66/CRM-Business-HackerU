f58701bda1a0151f938b019264a3e641b475a9bf
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2022-07-30 19:08:53 +0300

    git ignore

--- src/validation/card.validation.jsx
+++ src/validation/card.validation.jsx
@@ -9,7 +9,7 @@ const cardSchema = {
     .max(15)
     .required()
     .regex(/^0[2-9][-]?\d{7,9}$|^05[0-9][-]?\d{7,9}$|^07[7,3][-]?\d{7,9}$/),
-  bizImage: Joi.string().uri().min(20),
+  bizImage: Joi.string().allow("").uri().min(20),
 };
 
 export default cardSchema;
